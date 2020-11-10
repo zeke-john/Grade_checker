a08f8fefa760786638f95f98856b2fc10b985ae4
Zeke John <zekejohn118@gmail.com>
2020-11-09 21:58:20 -0800

    not as big of an oof

--- main.py
+++ main.py
@@ -1,50 +1,27 @@
-check = True
-jab = input("\nTest Weight: ")
+Test = True
 
-if jab.isnumeric()== False:
-        print("\nuse numbers")
-        quit()
+user_input_test_weight = input("\nTest Weight(divide by 100): ")
+test_weight = float(user_input_test_weight)
 
+user_input_test_score = input("Test Score: ")
+test_score = float(user_input_test_score)
 
+test_final_score = test_score*test_weight
 
-job = input("Test Score: ")
-test_weight = float(jab)
+#----------------------------------------------------------------------------------------
+Assignment = True
 
-if job.isnumeric() == False:
-        print("\nuse numbers")
-        quit()
+user_input_assignment_weight = input("\nAssignment Weight(divide by 100): ")
+user_input_assignment_score= input("Assignment Score: ")
 
-test_worth = test_weight/100
-test_score = float(job)
+assignment_weight = float(user_input_assignment_score)
+assignment_score = float(user_input_assignment_score)
 
-test_final_score = test_score*test_worth
+assignment_final_score = assignment_score*assignment_weight
 
-
-chock = True
-jyb = input("\nAssignment Weight: ")
-
-if jyb.isnumeric()== False:
-        print("\nuse numbers)")
-        quit()
-
-job = input("Assignment Score: ")
-
-
-assignment_weight = float(jab)
-assignment_worth = assignment_weight/100
-
-if job.isnumeric() == False:
-        print("\nuse numbers")
-        quit()
-
-
-assignment_score = float(job)
-
-assignment_final_score = assignment_score*assignment_worth
-
-if check and chock == True:
-                final_grade = assignment_final_score +test_final_score
-                print( str("\nfinal grade: ") + str(final_grade))
+if Test and Assignment == True:
+                final_grade = assignment_final_score+test_final_score
+                print( str("final grade: ") + str(final_grade))
 
 
 
