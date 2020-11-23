da962391ed8e55571f3b02c30fa80d38869361be
Zeke John <zekejohn118@gmail.com>
2020-11-23 14:22:25 -0800

    hanfled floats and bad entrys

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 user_input_parts_grade = input("\nHow many parts is your grade divivded in: ")
 
 while user_input_parts_grade.isnumeric() == False:
+        print("\ninvalid entry")
         user_input_parts_grade = input("\nHow many parts is your grade divivded in: ")
         
 
@@ -14,21 +15,21 @@ while y < user_parts_grade:
         user_input_no_Assignments = input("\nHow many Assignments have you taken for this part: ")
 
         while user_input_no_Assignments.isnumeric() == False:
+                print("\ninvalid entry")
                 user_input_no_Assignments = input("\nHow many Assignments have you taken for this part: ")
-
-                
 
         no_Assignments = int(user_input_no_Assignments)
 
-
         user_input_Assignments_weight = input("\nAssignments Weight: ")
-        
-        while user_input_Assignments_weight.isnumeric() == False:
-                user_input_Assignments_weight = input("\nAssignments Weight: ")
-                
 
-        user_weight = float(user_input_Assignments_weight)
-        Assignments_weight = user_weight/100
+
+        try:
+                user_weight = float(user_input_Assignments_weight)
+                Assignments_weight = user_weight/100
+
+        except:
+                print("\ninvalid entry")
+                user_input_Assignments_weight = input("\nAssignments Weight: ")
 
         x = 0
 
@@ -37,8 +38,8 @@ while y < user_parts_grade:
         while x < no_Assignments:
 
                 user_input_Assignments_score = input("\nScore #%d: " %(x+1))
-
-                if user_input_Assignments_score == float:
+                
+                try:
                         user_Assignments_score = float(user_input_Assignments_score)
                         x +=1
                         user_score_list.append(user_Assignments_score)
@@ -46,18 +47,13 @@ while y < user_parts_grade:
                         add_total_Assignments = sum(user_score_list)
                         mult_by_weight = add_total_Assignments*Assignments_weight
                         add_total_Assignments = mult_by_weight/no_Assignments
-
-
-                user_Assignments_score = float(user_input_Assignments_score)
-                x +=1
-                user_score_list.append(user_Assignments_score)
-
-                add_total_Assignments = sum(user_score_list)
-                mult_by_weight = add_total_Assignments*Assignments_weight
-                add_total_Assignments = mult_by_weight/no_Assignments
+                except:
+                        print("\ninvalid entry")
+                        
         y += 1
 
         total_list.append(add_total_Assignments)
         add_total_grade = sum(total_list)
-        
-print(str("\nTotal Grade: ") + str(add_total_grade) + str("%"))
+
+        if y == user_parts_grade:
+                print(str("\nTotal Grade: ") + str(add_total_grade) + str("%"))
